fd4077a208f516746da95b8ec5c4b313799384bc
Harish-Rusum <harish.rusum@gmail.com>
2024-11-16 21:13:32 +0530

    optimized buttons

--- utils/menu.py
+++ utils/menu.py
@@ -1,61 +1,58 @@
+
 import pygame
 class Menu:
     def __init__(self, surf):
         self.menuOpen = False
+        self.surf = surf
         self.overlay = pygame.Surface(surf.get_size(), pygame.SRCALPHA)
         self.overlay.fill((0, 0, 0, 200))
-        self.surf = surf
         self.holdingEsc = False
-        self.buttons = [
-            pygame.image.load("assets/Buttons/back.png").convert_alpha(),
-            pygame.image.load("assets/Buttons/exit.png").convert_alpha(),
-            pygame.image.load("assets/Buttons/mute.png").convert_alpha(),
-            pygame.image.load("assets/Buttons/unmute.png").convert_alpha(),
-            pygame.image.load("assets/Buttons/volUp.png").convert_alpha(),
-            pygame.image.load("assets/Buttons/volDown.png").convert_alpha(),
+
+        rawButtons = [
+            "back.png", "exit.png", "mute.png", "unmute.png", "volUp.png", "volDown.png"
         ]
+        self.buttons = [
+            pygame.transform.smoothscale(
+                pygame.image.load(f"assets/Buttons/{name}").convert_alpha(),
+                (118 * 1.5, 16*1.5)
+            ) for name in rawButtons
+        ]
+
         self.exit = False
         self.mute = False
+        self.buttonStates = [
+            (self.buttons[:3] + self.buttons[4:]),
+            (self.buttons[:2] + self.buttons[3:])
+        ]
+
         self.muteHeld = False
         self.volUpHeld = False
         self.volDownHeld = False
         self.soundChange = 0
-        self.directions = [
-            (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2),
-            (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2),
-            (0, -2), (0, -1), (0, 1), (0, 2),
-            (1, -2), (1, -1), (1, 0), (1, 1), (1, 2),
-            (2, -2), (2, -1), (2, 0), (2, 1), (2, 2),
-        ]
+
+        self.directions = [(dx, dy) for dx in range(-2, 3) for dy in range(-2, 3) if (dx, dy) != (0, 0)]
 
     def outline(self, img, pos, color=(255, 255, 255)):
         mask = pygame.mask.from_surface(img)
         outline_points = mask.outline()
-
-        for offset in self.directions:
-            for point in outline_points:
-                x, y = point
-                outlinePos = (pos[0] + x + offset[0], pos[1] + y + offset[1])
-                self.overlay.set_at(outlinePos, color)
+        for dx, dy in self.directions:
+            for x, y in outline_points:
+                self.overlay.set_at((pos[0] + x + dx, pos[1] + y + dy), color)
 
     def click(self, i):
         if i == 0:
             self.menuOpen = False
         elif i == 1:
             self.exit = True
-        elif i == 2:
-            if not self.muteHeld:
-                self.mute = not self.mute
-                self.muteHeld = True
-        elif i == 3:
-            if not self.volUpHeld:
-                self.soundChange += 0.1
-                self.volUpHeld = True
-        elif i == 4:
-            if not self.volDownHeld:
-                self.soundChange -= 0.1
-                self.volDownHeld = True
-        return
+        elif i == 2 and not self.muteHeld:
+            self.mute = not self.mute
+            self.muteHeld = True
+        elif i == 3 and not self.volUpHeld:
+            self.soundChange += 0.1
+            self.volUpHeld = True
+        elif i == 4 and not self.volDownHeld:
+            self.soundChange -= 0.1
+            self.volDownHeld = True
 
     def render(self):
         mouseX, mouseY = pygame.mouse.get_pos()
@@ -63,30 +60,30 @@ class Menu:
 
         if keys[pygame.K_ESCAPE]:
             if not self.holdingEsc:
-                self.holdingEsc = True
                 self.menuOpen = not self.menuOpen
+                self.holdingEsc = True
         else:
             self.holdingEsc = False
 
-        self.overlay.fill((0, 0, 0, 200))
-
-        for i, button in enumerate(self.buttons[:3] + self.buttons[4:] if not self.mute else self.buttons[:2] + self.buttons[3:]):
-            button_scaled = pygame.transform.smoothscale(button, (button.get_width() * 1.5, button.get_height() * 1.5))
-            button_x, button_y = 20, (i * 60 + 20)
-            if button_x <= mouseX <= button_x + button_scaled.get_width() and \
-               button_y <= mouseY <= button_y + button_scaled.get_height():
-                self.outline(button_scaled, (button_x, button_y))
-                if self.menuOpen and pygame.mouse.get_pressed()[0]:
-                    self.click(i)
-            else:
-                self.outline(button_scaled, (button_x, button_y), color=(100, 100, 100))
-
-            self.overlay.blit(button_scaled, (button_x, button_y))
-
-        if not pygame.mouse.get_pressed()[0]:
-            self.muteHeld = False
-            self.volUpHeld = False
-            self.volDownHeld = False
-
         if self.menuOpen:
+            self.overlay.fill((0, 0, 0, 200))
+
+            active_buttons = self.buttonStates[self.mute]
+            for i, button in enumerate(active_buttons):
+                button_x, button_y = 20, (i * 50 + 20)
+                button_rect = pygame.Rect(button_x, button_y, button.get_width(), button.get_height())
+
+                if button_rect.collidepoint(mouseX, mouseY):
+                    self.outline(button, (button_x, button_y))
+                    if pygame.mouse.get_pressed()[0]:
+                        self.click(i)
+                else:
+                    self.outline(button, (button_x, button_y), color=(100, 100, 100))
+
+                self.overlay.blit(button, (button_x, button_y))
+
+
+            if not pygame.mouse.get_pressed()[0]:
+                self.muteHeld = self.volUpHeld = self.volDownHeld = False
+
             self.surf.blit(self.overlay, (0, 0))
